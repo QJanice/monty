8ec43c011e2022cf889fc1ebec7a18dc4114ff6c
QJanice <queenobiekwuye@gmail.com>
2023-01-26 06:36:58 -0800

    fix

--- sub.c
+++ sub.c
@@ -10,7 +10,7 @@ void f_sub(stack_t **head, unsigned int counter)
 	stack_t *aux;
 	int sus, nodes;
 
-	aux = *head
+	aux = *head;
 	for (nodes = 0; aux != NULL; nodes++)
 		aux = aux->next;
 	if (nodes < 2)
